0c4133c1d38f1da05dced6df38603fcd845eeb42
mmiguelo <marco_miguelote@hotmail.com>
2025-01-27 09:16:09 +0000

    defined errors on .h and started making main

--- includes/pipex.h
+++ includes/pipex.h
@@ -6,7 +6,7 @@
 /*   By: mmiguelo <mmiguelo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/24 10:34:18 by mmiguelo          #+#    #+#             */
-/*   Updated: 2025/01/24 10:59:34 by mmiguelo         ###   ########.fr       */
+/*   Updated: 2025/01/27 09:10:55 by mmiguelo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,24 @@
 # define PIPEX
 
 # include "../my_libft/libft.h"
-#include <stdlib.h>
-#include <fcntl.h>
 #include <stdio.h>
-
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <errno.h>
 
 /*=============================================================================#
 #                               DEFINES                                        #
 #=============================================================================*/
 
-/*=============================================================================#
-#                               STRUCTS                                        #
-#=============================================================================*/
+# define PIPE_ERROR "An error ocurred with opening the pipe\n"
+# define FORK_ERROR "An error ocurred with making a fork"
 
 /*=============================================================================#
 #                               GENERAL                                        #
 #=============================================================================*/
+
 
 #endif
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: mmiguelo <mmiguelo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/24 10:49:48 by mmiguelo          #+#    #+#             */
-/*   Updated: 2025/01/24 10:56:31 by mmiguelo         ###   ########.fr       */
+/*   Updated: 2025/01/27 09:14:52 by mmiguelo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,19 @@
 
 int main(int argc, char **argv)
 {
+	int		fd[2];
+	// fd[0] -> read
+	// fd[1] -> write
+	pid_t	id;
 
+	if (argc == 5)
+	{
+		pipe(fd);
+		if (pipe(fd) == -1)
+			perror(PIPE_ERROR);
+		id = fork();
+		if (id == -1)
+			perror(FORK_ERROR);
+	}
+	return (0);
 }
