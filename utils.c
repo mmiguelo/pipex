f5b0f229650d7642e87e8c281a37a771fec4b91d
mmiguelo <marco_miguelote@hotmail.com>
2025-02-06 17:44:38 +0000

    clear norminette errors

--- utils.c
+++ utils.c
@@ -6,7 +6,7 @@
 /*   By: mmiguelo <mmiguelo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/06 11:22:46 by mmiguelo          #+#    #+#             */
-/*   Updated: 2025/02/06 17:19:14 by mmiguelo         ###   ########.fr       */
+/*   Updated: 2025/02/06 17:44:26 by mmiguelo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,7 @@ void	ft_free(char **str)
 int	parse(char **envp, char **argv)
 {
 	int	i;
-	
+
 	if (!envp || !*envp)
 	{
 		ft_putstr_fd(ENVP_ERROR, 2);
@@ -43,7 +43,8 @@ int	parse(char **envp, char **argv)
 		exit(2);
 	}
 	i = 0;
-	if (!argv[2] || (argv[2] && !argv[2][0]) || !argv[3] || (argv[3] && !argv[3][0]))
+	if (!argv[2] || (argv[2] && !argv[2][0])
+			|| !argv[3] || (argv[3] && !argv[3][0]))
 	{
 		ft_putstr_fd("Wrong input. Try ./pipex file1 cmd1 cmd2 file2\n", 2);
 		exit(3);
