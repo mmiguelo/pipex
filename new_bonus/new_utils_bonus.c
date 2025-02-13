a22c6d8bffa5b4089538a7bff1ddfb47543efd7e
mmiguelo <marco_miguelote@hotmail.com>
2025-02-13 21:24:34 +0000

    added parse function to utils

--- new_bonus/new_utils_bonus.c
+++ new_bonus/new_utils_bonus.c
@@ -11,3 +11,32 @@
 /* ************************************************************************** */
 
 #include "pipex_bonus.h"
+
+void	parse_bonus(char **envp, char **argv)
+{
+	int	i;
+
+	if (!envp || !*envp)
+	{
+		ft_putstr_fd(ENVP_ERROR, 2);
+		exit(1);
+	}
+	i = 0;
+	while (envp[i] && ft_strnstr(envp[i], "PATH=", 5) == 0)
+		i++;
+	if (!envp[i])
+	{
+		ft_putstr_fd(PATH_ERROR, 2);
+		exit(2);
+	}
+	i = -1;
+	while (argv[++i])
+	{
+		if (argv[i] && !argv[i][0])
+		{
+			ft_putstr_fd(INPUT2_ERROR, 2);
+			exit(3);
+		}
+	}
+	return ;
+}
