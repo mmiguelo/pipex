8c3e4702a359903e2a2c11b28519135d4d5d474d
mmiguelo <marco_miguelote@hotmail.com>
2025-02-13 21:50:39 +0000

    added code inside the if !here_doc

--- new_bonus/new_pipex_bonus.c
+++ new_bonus/new_pipex_bonus.c
@@ -14,17 +14,25 @@
 
 int main(int ac, char **av, char **envp)
 {
-    t_pipex pipes;
+	t_pipex	pipes;
+	int		fd_in;
+	int		fd_out;
 
-    if (av[1] && (ft_strncmp(av[1], "here_doc", 8) == 0 && ac < 6))
-        return (ft_putstr_fd(INPUT_ERROR, 2), 1)
-    if (ac < 5)
-        return (ft_putstr_fd(INPUT2_ERROR, 2), 2)
-    parse_bonus(envp, av);
-    if (!pipes.here_doc)
-    {
-
-    }
-    else
-        start_here_doc(&pipes);
+	if (av[1] && (ft_strncmp(av[1], "here_doc", 8) == 0 && ac < 6))
+		return (ft_putstr_fd(INPUT_ERROR, 2), 1)
+	if (ac < 5)
+		return (ft_putstr_fd(INPUT2_ERROR, 2), 2)
+	parse_bonus(envp, av);
+	if (!pipes.here_doc)
+	{
+		fd_in = open_file(av[1], 2);
+		fd_out = open_file(av[ac -1], 1);
+		dup2(fd_in, STDIN_FILENO);
+		while (i < ac - 2)
+			main_process(&av[pipes.cmd++], envp);
+		dup2(fd_out, STDOUT_FILENO);
+		process(av[ac - 2], envp, NULL);
+	}
+	else
+		start_here_doc(&pipes);
 }
